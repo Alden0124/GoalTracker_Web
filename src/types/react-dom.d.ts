e265c010bdc547bd6ceef8759af02c4d85f59f4c
Alden0124 <hsuchiahung24@gmail.com>
2024-12-03 12:36:29 +0800

    調整部屬錯誤問題

--- src/types/react-dom.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "react-dom" {
-  export * from "react-dom/client";
-  export const createPortal: (children: React.ReactNode, container: Element | DocumentFragment) => React.ReactPortal;
-}
-
-declare module "react-dom/client" {
-  import type { Root } from "react-dom/client";
-  export function createRoot(container: Element | null): Root;
-}
