5880de1227684668a305b9c80afc91f8a89a27fb
Alden0124 <hsuchiahung24@gmail.com>
2024-12-03 12:58:07 +0800

    調整vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,31 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react()],
-    define: {
-      "import.meta.env": JSON.stringify(env),
-    },
-    resolve: {
-      alias: [
-        {
-          find: "@",
-          replacement: path.resolve(process.cwd(), "src"),
-        },
-      ],
-    },
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            vendor: ["react", "react-dom"],
-          },
-        },
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(process.cwd(), "src"),
       },
-    },
-    server: {
-      port: 10000,
-      host: "0.0.0.0",
-      proxy: {
-        "/api": {
-          target: process.env.VITE_API_URL || "http://localhost:3001/",
-          changeOrigin: true,
-        },
-      },
-    },
-    preview: {
-      port: 10000,
-      host: "0.0.0.0",
-    },
-  };
+    ],
+  },
+  server: {
+    port: 10000,
+    host: "0.0.0.0",
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001/',
+        changeOrigin: true,
+      }
+    }
+  },
+  preview: {
+    port: 10000,
+    host: "0.0.0.0",
+  },
 });
+
